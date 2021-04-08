d0c30b0e8824c72840c2e015763308a02ed03618
loiola0 <victorloiola@alu.ufc.br>
2021-04-08 00:45:45 -0300

    Aula sobre Renderização condicional

--- módulo-20/meusite/src/App.js
+++ módulo-20/meusite/src/App.js
@@ -1,11 +1,43 @@
 import React,{Component} from 'react';
-import Member from './components/Member';
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            status: true
+        };
+        
+        this.entrar = this.entrar.bind(this);
+        this.sair = this.sair.bind(this);
+    }
+
+    entrar(){
+        this.setState({status: true});
+    }
+
+    sair(){
+        this.setState({status: false});
+    }
 
     render(){
         return(
             <div>
-               <Member name="Visitante"/>
+
+                {this.state.status ? 
+                    <div>
+                        <h2>Bem-Vindo ao Sistema!</h2>
+                        <button onClick={this.sair}>Sair</button>
+                    </div> :
+                    <div>
+                        <h2>Olá visitante! Faça seu login</h2>
+                        <button onClick={this.entrar}>Entrar</button>
+                    </div>
+                }
+                
+                <div>
+                    <h2>Curso React Js</h2>
+                </div>
+                
             </div>
         );
     }
