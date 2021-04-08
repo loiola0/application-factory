66a771acc867517c01005fdc0b9d5f07bb8ebf38
loiola0 <victorloiola@alu.ufc.br>
2021-04-08 01:28:43 -0300

    Aula trabalhando com listas

--- módulo-20/meusite/src/App.js
+++ módulo-20/meusite/src/App.js
@@ -1,44 +1,33 @@
 import React,{Component} from 'react';
+import Feed from './components/feed';
+
 class App extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            status: true
+            feed:[
+                {id:1,username:"Victor",likes:130,comments:14},
+                {id:2,username:"AuucuoGelson",likes:240,comments:9},
+                {id:3,username:"John Wick",likes:30,comments:2},
+                {id:4,username:"Paula",likes:1,comments:1},
+            ]
         };
         
-        this.entrar = this.entrar.bind(this);
-        this.sair = this.sair.bind(this);
-    }
-
-    entrar(){
-        this.setState({status: true});
-    }
-
-    sair(){
-        this.setState({status: false});
+       
     }
 
     render(){
+       
         return(
             <div>
-
-                {this.state.status ? 
-                    <div>
-                        <h2>Bem-Vindo ao Sistema!</h2>
-                        <button onClick={this.sair}>Sair</button>
-                    </div> :
-                    <div>
-                        <h2>Olá visitante! Faça seu login</h2>
-                        <button onClick={this.entrar}>Entrar</button>
-                    </div>
-                }
-                
-                <div>
-                    <h2>Curso React Js</h2>
-                </div>
-                
-            </div>
+                {this.state.feed.map((item)=>{
+                    return(
+                        <Feed key={item.id} username={item.username} likes={item.likes} comments={item.comments}/>
+                    );
+                    
+                })}
+            </div> 
         );
     }
 }
