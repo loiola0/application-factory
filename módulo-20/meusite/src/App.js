289a87984eee1da40ac22acc6854f53eca7643fc
loiola0 <victorloiola@alu.ufc.br>
2021-04-09 00:54:12 -0300

    Aula Formularios parte 2

--- módulo-20/meusite/src/App.js
+++ módulo-20/meusite/src/App.js
@@ -6,23 +6,23 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            email: "",
-            senha: "",
-            sexo: "Masculino"
-        }
-        this.trocaEmail = this.trocaEmail.bind(this);
-        this.trocaSexo = this.trocaSexo.bind(this);
+            form: {
+                nome:'',
+                email:'',
+                senha:'',
+                sexo:'',
+            },
+        };
+
+        this.dadosForm = this.dadosForm.bind(this);
+        
     }
 
-    trocaEmail(e){
-        let valorDigitado = e.target.value;
-        this.setState({email:valorDigitado});
-    }
-
-    trocaSexo(e){
-        let valorDigitado = e.target.value;
-        this.setState({sexo:valorDigitado});
-    }
+    dadosForm(e){
+        let form = this.state.form;
+        form[e.target.name] = e.target.value; 
+        this.setState({form:form});
+    } 
 
     render(){
        
@@ -30,22 +30,28 @@ class App extends Component {
             <div>
                 <div>
                     <h2>Login</h2>
+                    Nome:
+                        <input type="text" name="nome" value={this.state.form.nome}
+                            onChange={this.dadosForm}/><br/>
                     Email:
-                        <input type="email" name="email" value={this.state.email}
-                            onChange={this.trocaEmail}/><br/>
+                        <input type="email" name="email" value={this.state.form.email}
+                            onChange={this.dadosForm}/><br/>
                     Senha:
-                        <input type="password" name="senha" value={this.state.senha}
-                        onChange={(e)=>{this.setState({senha:e.target.value})}}/><br/>
+                        <input type="password" name="senha" value={this.state.form.senha}
+                        onChange={this.dadosForm}/><br/>
                     Sexo:
-                      <select value={this.state.sexo} onChange={this.trocaSexo}>
+                      <select name="senha" value={this.state.form.sexo} onChange={this.dadosForm}>
                         <option value="Masculino">Masculino</option>
                         <option value="Feminino">Feminino</option>
                       </select>
+                    
+                
                 </div>
                 <div>
-                    <h3>{this.state.email}</h3>
-                    <h3>{this.state.senha}</h3>
-                    <h3>{this.state.sexo}</h3>
+                    <h3>{this.state.form.nome}</h3>
+                    <h3>{this.state.form.email}</h3>
+                    <h3>{this.state.form.senha}</h3>
+                    <h3>{this.state.form.sexo}</h3>
                 </div>
                
             </div> 
