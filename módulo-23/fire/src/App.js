2930c97a901bdd87f483c8613e64bc203c6f0c26
loiola0 <victorloiola@alu.ufc.br>
2021-04-25 00:39:07 -0300

    Aula de monitorar listas,editando item,excluindo item foi concluídas

--- módulo-23/fire/src/App.js
+++ módulo-23/fire/src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import firebase from './firebaseConnection';
 import './style.css';
 
@@ -6,9 +6,36 @@ import './style.css';
 
 function App() {
 
+    const [idPost,setIdPost] = useState('');
     const [titulo,setTitulo] = useState('');
     const [autor,setAutor] = useState('');
     const [posts,setPosts] = useState([]);
+
+
+    useEffect(()=>{
+
+        async function loadPosts(){
+          await firebase.firestore().collection('posts')
+          .onSnapshot((doc)=>{
+
+            let meusPosts = [];
+
+              doc.forEach((item)=>{
+                meusPosts.push({
+                    id: item.id,
+                    titulo: item.data().titulo,
+                    autor: item.data().autor,
+                });
+              })
+
+              setPosts(meusPosts);
+          })
+        }
+
+        loadPosts();
+
+    },[]);
+
 
     async function handleAdd(){
       await firebase.firestore().collection('posts')
@@ -74,12 +101,40 @@ function App() {
 
     }
 
+     async function editarPost(){
+        await firebase.firestore().collection('posts')
+        .doc(idPost)
+        .update({
+            titulo: titulo,
+            autor: autor,
+        })
+        .then(()=>{
+          alert('Dados atualizados com sucesso!');
+          setAutor('');
+          setTitulo('');
+          setIdPost('');
+        })
+        .catch((error)=>alert('Erro ao atualizar: '+error))
+    }
+
+    async function excluirPost(id){
+      await firebase.firestore().collection('posts')
+      .doc(id)
+      .delete()
+      .then(()=> alert('Post excluído com sucesso!'))
+      .catch((error)=> alert('Não foi possível apagar o post: '+error))
+    }
+
   return (
     <div className="App">
       <h1>ReactJs + FireBase :)</h1><br/>
 
 
       <div className="container">
+
+        <label>ID: </label>
+        <input type="text" value={idPost} onChange={(e) => setIdPost(e.target.value)}/>
+
         <label>Titulo: </label>
         <textarea type="text" value={titulo} onChange={(e)=>setTitulo(e.target.value)}/>
 
@@ -88,14 +143,18 @@ function App() {
 
         <button onClick={handleAdd}>Cadastrar</button>
 
-        <button onClick={buscarPost}>Buscar Post</button><br/>
+        <button onClick={buscarPost}>Buscar Post</button>
+
+        <button onClick={editarPost}>Editar Post</button><br/>
 
         <ul>
           {posts.map((post)=>{
             return(
               <li key={post.id}>
+                <span>Id: {post.id}</span><br/>
                 <span>Titulo: {post.titulo}</span><br/>
-                <span>Autor: {post.autor}</span><br/><br/>
+                <span>Autor: {post.autor}</span><br/>
+                <button onClick={() => excluirPost(post.id)}>Excluir post</button><br/><br/>
               </li>
               
             );
